9ad83159d27f42f3b0fdd2b899abf44d33009492
johndoejs <m8r-24jbrr@mailinator.com>
2017-01-05 18:15:17 +0000

    Added the main component here instead of putting the comp in a separate file

--- src/module.ts
+++ src/module.ts
@@ -1,10 +1,17 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import AppComponent from './app';
+import {NgModule,Component}	from '@angular/core';
+import {BrowserModule}		from '@angular/platform-browser';
+
+@Component({
+	selector: 'my-app',
+	template: `
+		<div>
+			{{ text }}
+		</div>
+	`
+}) export class App { text = 'Hello world!'; }
 
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [AppComponent],
-  bootstrap: [AppComponent]
-})
-export class AppModule {}
+  imports:		[BrowserModule],
+  declarations:	[App],
+  bootstrap:	[App]
+}) export class AppModule {}
